7a02542038f76a33826a574ce98cd41e549ee35e
Den Delimarsky <1389609+dend@users.noreply.github.com>
2022-07-13 18:00:38 -0700

    Ensure support for nullable types in stats

--- Grunt/Grunt/Models/HaloInfinite/PlayAssetStats.cs
+++ Grunt/Grunt/Models/HaloInfinite/PlayAssetStats.cs
@@ -2,13 +2,13 @@
 {
     public class PlayAssetStats
     {
-        public int PlaysRecent { get; set; }
-        public int PlaysAllTime { get; set; }
-        public int Favorites { get; set; }
-        public int Likes { get; set; }
-        public int Bookmarks { get; set; }
-        public int ParentAssetCount { get; set; }
-        public float AverageRating { get; set; }
-        public int NumberOfRatings { get; set; }
+        public int? PlaysRecent { get; set; }
+        public int? PlaysAllTime { get; set; }
+        public int? Favorites { get; set; }
+        public int? Likes { get; set; }
+        public int? Bookmarks { get; set; }
+        public int? ParentAssetCount { get; set; }
+        public float? AverageRating { get; set; }
+        public int? NumberOfRatings { get; set; }
     }
 }
