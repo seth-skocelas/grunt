6d203a041e31b20482dc69fabb62988b7a30deef
Den Delimarsky <1389609+dend@users.noreply.github.com>
2022-07-22 00:18:34 -0700

    Adding some missed files that need the proper attributes

--- Grunt/Grunt/Models/HaloInfinite/AcademyClientManifest.cs
+++ Grunt/Grunt/Models/HaloInfinite/AcademyClientManifest.cs
@@ -1,5 +1,6 @@
 ﻿namespace Grunt.Models.HaloInfinite
 {
+    [IsAutomaticallySerializable(IsReady = true)]
     public class AcademyClientManifest
     {
         public AcademyTutorial Tutorial { get; set; }
--- Grunt/Grunt/Models/HaloInfinite/AcademyDrill.cs
+++ Grunt/Grunt/Models/HaloInfinite/AcademyDrill.cs
@@ -1,5 +1,6 @@
 ﻿namespace Grunt.Models.HaloInfinite
 {
+    [IsAutomaticallySerializable(IsReady = true)]
     public class AcademyDrill
     {
         public string TitleStringID { get; set; }
