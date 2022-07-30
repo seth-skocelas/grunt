c8a894cb35e33aa6bf84b6fb2ecbf76ec6afa5f7
Seth Skocelas <s.skocelas@gmail.com>
2022-07-29 19:39:10 -0500

    Added RankedRewards handling

---
+++ Grunt/Grunt/Models/HaloInfinite/RankedRewards.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="RankedRewards.cs" company="Den Delimarsky">
+// Developed by Den Delimarsky.
+// Den Delimarsky licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// The underlying API powering Grunt is managed by 343 Industries and Microsoft. This wrapper is not endorsed by 343 Industries or Microsoft.
+// </copyright>
+
+using System.Collections.Generic;
+
+namespace OpenSpartan.Grunt.Models.HaloInfinite
+{
+    [IsAutomaticallySerializable]
+    public class RankedRewards
+    {
+        public string RewardId { get; set; }
+        public Dictionary<string, string> AwardedRewards { get; set; }
+    }
+}
--- Grunt/Grunt/Models/HaloInfinite/SkillResult.cs
+++ Grunt/Grunt/Models/HaloInfinite/SkillResult.cs
@@ -18,8 +18,7 @@ namespace OpenSpartan.Grunt.Models.HaloInfinite
         public StatPerformances StatPerformances { get; set; }
         public int TeamId { get; set; }
         public Dictionary<string, double> TeamMmrs { get; set; }
-        //TODO: Investigate that RankedRewards is actually a string. My hunch is that this will be an array.
-        public string RankedRewards { get; set; }
+        public RankedRewards RankedRewards { get; set; }
         public Counterfactuals Counterfactuals { get; set; }
 
     }
